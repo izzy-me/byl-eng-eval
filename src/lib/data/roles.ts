e19018f0c696cbb276cd0196c3dd8dbcd376f05c
izzymeyerson <imeyerso@stanford.edu>
2026-01-20 15:02:11 -0800

    modigied roles.ts file, created new API endpoint file

--- src/lib/data/roles.ts
+++ src/lib/data/roles.ts
@@ -1,7 +1,19 @@
 import raw from "@/data/roles.json";
 import { Role, RoleId } from "@/lib/types";
 
-const ROLES = raw as Role[];
+
+//redefined const ROLES because there was a mismatch between this and the JSON keys
+const ROLES: Role[] = (raw as any[]).map((r) => ({
+  id: r.id,
+  name: r.name,
+  role_desc: r.role_desc,
+  core_drive: r.core_drive,
+  most_like_when: r.most_like_when,
+  high_rank_desc: r.core_rank_desc,
+  low_rank_desc: r.peripheral_rank_desc,
+  top_rank_desc: r.top_rank_desc,
+  bottom_rank_desc: r.bottom_rank_desc,
+}));
 
 export function getAllRoles(): Role[] {
   return ROLES;
